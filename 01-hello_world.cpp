e4e935e2091ae22def0bd78ee9141654a75e6ae9
Lucas Mazoni Guerra <lucasmazoniguerra@gmail.com>
2023-10-10 20:58:09 -0300

    up

---
+++ 01-hello_world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Hello World! \n";
+    cout << "My name is Lucas.\n";
+    cout << "I'm learning C++!";
+
+    return 0;
+}
---
+++ 02-comments.cpp
@@ -0,0 +1,15 @@
+//single line comment
+
+/*
+Multiple line 
+comment
+!!!
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Testing comments";
+}
