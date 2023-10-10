f0b5685f14e5c3d571e7bf574a31cce82ab5d3a5
Lucas Mazoni Guerra <lucasmazoniguerra@gmail.com>
2023-10-10 20:46:24 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+//comment
+using namespace std;
+
+int main(){
+    std::cout << "Hello World! \n";
+    std::cout << "My name is Lucas.\n";
+    std::cout << "I'm learning C++!";
+
+    return 0;
+}
