9fe61be190c72589bc8cfe6f02b37be1a4ab1218
Lucas Mazoni Guerra <lucasmazoniguerra@gmail.com>
2023-10-12 17:04:43 -0300

    up

---
+++ 05-numbers_systems.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number1 = 15; //decimal representation
+    int number2 = 017; //octal representation
+    int number3 = 0x0f; //hexadecimal representation
+    int number4 = 0b1111; //binary representation
+
+    cout << "number 1 = " << number1 << "\n";
+    cout << "number 2 = " << number2 << "\n";
+    cout << "number 3 = " << number3 << "\n";
+    cout << "number 4 = " << number4 << "\n";
+
+
+    return 0;
+}
