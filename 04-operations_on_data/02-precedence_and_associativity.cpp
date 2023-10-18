4bdc76db7ffb889a659d842363be37445637fc3f
Lucas Mazoni Guerra <lucasmazoniguerra@gmail.com>
2023-10-18 10:19:10 -0300

    up

---
+++ 04-operations_on_data/02-precedence_and_associativity.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int a {1};
+    int b {2};
+    int c {3};
+    int d {4};
+    int e {5};
+    int f {6};
+    int g {7};
+    int soma = ((a + b) * ( g - c)) / (d + e - f);
+
+    cout << soma; 
+
+    return 0;
+}
