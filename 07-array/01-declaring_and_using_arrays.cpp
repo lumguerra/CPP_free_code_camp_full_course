b8ce64c7a0edd22fdc03b41fc4bedaf477734e0a
Lucas Mazoni Guerra <lucasmazoniguerra@gmail.com>
2023-10-19 17:19:18 -0300

    up

--- 07-array/01-declaring_and_using_arrays.cpp
+++ 07-array/01-declaring_and_using_arrays.cpp
@@ -6,9 +6,21 @@ int main(){
     int salary[10] {};
     const size_t COUNT {10};
 
+    //for loop reading array times 10
     for( size_t i{0}; i < COUNT; ++i ){
-        cout << "salary[" << i + 1 << "]: " << i*10 << "\n";
-    } 
+        cout << "salary[" << i << "]: " << i*10 << "\n";
+    }
+    cout << "------------------------------------\n";
+
+    //for range loop to sum elements in a array
+       
+    double notes[] {5.6,4.0,23.4,6.6};
+    double sum {};
+
+    for ( double n : notes ){
+        sum += n;
+    }
+    cout << "the sum of the notes is: " << sum << "\n";
 
     return 0;
 }
