6c03b464f5f8fea598360de6c652f6ef8ebe5c9d
Lucas Mazoni Guerra <lucasmazoniguerra@gmail.com>
2023-10-25 17:18:01 -0300

    up

---
+++ 11-functions/03-multiple_files/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "functions.h"
+using namespace std;
+
+int main()
+{
+    cout << "max: " << max(4,5) << "\n";
+    cout << "min: " << min(4,5) << "\n";
+    cout << "add_and_mult: " << add_and_mult(4,5) << "\n";
+
+    return 0;
+}
