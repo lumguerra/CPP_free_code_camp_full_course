f7f2643a04921d45cf5c38d650f7a5d53a13c4f3
Lucas Mazoni Guerra <lucasmazoniguerra@gmail.com>
2023-10-26 17:02:05 -0300

    up

--- 14-lambda_functions/03-capture_all_lists/main.cpp
+++ 14-lambda_functions/03-capture_all_lists/main.cpp
@@ -3,6 +3,19 @@ using namespace std;
 
 int main(){
     
+    int a{4};
+    int b{9};
+    int c{4};
+
+    auto func = [&](){
+        c = a * b;
+        cout << "c= " << c << "\n";
+    };
+
+    cout << a + b + c << "\n";
+    func();
+    cout << c << "\n";
+    cout << a + b + c << "\n";
 
     return 0;
 }
