041330416b772bc07b0faaca2092a10bff5d8483
Lucas Mazoni Guerra <lucasmazoniguerra@gmail.com>
2023-10-26 16:22:24 -0300

    up

--- 14-lambda_functions/02-capture_lists/main.cpp
+++ 14-lambda_functions/02-capture_lists/main.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main(){
     
+    int c{8};
+
+    auto func = [&c](){
+        cout << "inner-c value= " << c * 100 << " inner-c&: " << &c << "\n";
+    };
+
+    for( size_t i{}; i < 5; ++i ){
+        cout << "outter-c value= " << c + i << " outter-c&: " << &c << "\n";
+        func();
+    }
 
     return 0;
 }
