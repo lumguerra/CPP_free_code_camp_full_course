323c8f9ef0704fb7c2f49f65a677a2d1eb562594
Lucas Guerra <lucasmazoniguerra@gmail.com>
2023-11-15 17:34:53 -0300

    up

--- 01-random_topics/list_containers/main.cpp
+++ 01-random_topics/list_containers/main.cpp
@@ -3,6 +3,29 @@
 
 using namespace std;
 
+class YoutubeChannel {
+    private:
+        string Name;
+        string ChannelName;
+        int SubscriberCount;
+        list<string> PublishedVideoTitles; 
+
+    public:
+        YoutubeChannel(string_view p_Name, string_view p_ChannelName){
+            Name = p_Name;
+            ChannelName = p_ChannelName;
+        }
+        void GetInfo(){
+            cout << "Name: " << Name << "\n";
+            cout << "Channel name: " << ChannelName << "\n";
+            cout << "subscriber count: " << SubscriberCount << "\n";
+            cout << "Videos: " << "\n";
+            for (string videoTitle: PublishedVideoTitles){
+                cout << videoTitle << "\n";
+            }
+        }
+};
+
 int main(){
 
     list<string>Names;
