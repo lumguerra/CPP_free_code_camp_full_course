5f953f598b9f3875ce6761747eb5198195c0a3cd
Lucas Guerra <lucasmazoniguerra@gmail.com>
2023-11-22 18:15:06 -0300

    up

--- 18-inheritance/03-bass_class_acesses_specifiers/main.cpp
+++ 18-inheritance/03-bass_class_acesses_specifiers/main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include "person.h"
+#include "player.h"
+
 using namespace std;
 
 int main(){
 
     Person Person1("Lucas Mazoni Guerra", 34, "Av. Ordem e Progresso, 1190");
-    cout << "Person1: " << Person1;
+    cout << "Person1: " << Person1 << '\n';
+
+    Player player1;
+    player1.m_full_name = "Sarah Guerra";
+
+    cout << player1;
+
     
     return 0;
 }
