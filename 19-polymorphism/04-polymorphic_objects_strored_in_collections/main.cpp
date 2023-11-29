5a2ffed1419500116704dc96202ecc7ecc91dc42
Lucas Guerra <lucasmazoniguerra@gmail.com>
2023-11-29 16:20:26 -0300

    up

--- 19-polymorphism/04-polymorphic_objects_strored_in_collections/main.cpp
+++ 19-polymorphism/04-polymorphic_objects_strored_in_collections/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <memory>
 #include "shape.h"
 #include "oval.h"
 #include "circle.h"
@@ -7,14 +9,14 @@ using namespace std;
 
 int main(){
 
-    Shape shape1;
-    Oval oval1;
-    Circle circle1;
+    Oval oval1(44, 5.9, "oval1"), oval2(44, 5.9, "oval2") ,oval3(6.6, 5, "oval3");
+    Circle circle1( 5.5, "circle1"), circle2( 2, "circle2"), circle3( 7.8, "circle3");
 
-    int arr[3];
+    shared_ptr<Shape> shapes2[]{make_shared<Oval>(oval1, oval2, oval3), make_shared<Circle>(circle1, circle2, circle3)};
 
-    cout << sizeof(shape1) << "\n";
-    cout << size(arr);
+    for(Shape& s : shapes2 ){
+        s.draw();
+    }
 
     return 0;
 }
